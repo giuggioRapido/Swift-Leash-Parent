41d743e494c0974f634b79f372812518b5c5de59
Chickenpup <christophergshea@gmail.com>
2016-01-12 13:20:37 -0500

    Initial Commit

---
+++ Swift Digital Leash-Parent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Digital Leash-Parent
+//
+//  Created by Chris on 1/12/16.
+//  Copyright © 2016 Prince Fungus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
